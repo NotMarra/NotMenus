fc995fc826be57f151fc60d26d18b241a9c1f57f
NotMarra <marra313cz@gmail.com>
2024-11-05 01:09:46 +0100

    Update ClickEvents.java

--- src/main/java/com/notmarra/notmenus/utils/ClickEvents.java
+++ src/main/java/com/notmarra/notmenus/utils/ClickEvents.java
@@ -7,22 +7,26 @@ import java.util.List;
 
 public class ClickEvents {
     public static void clickEvent(List<String> commands, List<String> messages, Player player) {
-        for (String command : commands) {
-            if (command.contains("[close]")) {
-                player.closeInventory();
-            } else if (command.contains("[player]")) {
-                player.performCommand(command.replace("%player%", player.getName()));
-            } else if (command.contains("[console]")) {
-                NotMenus.getInstance().getServer().dispatchCommand(NotMenus.getInstance().getServer().getConsoleSender(), command.replace("%player%", player.getName()));
-            } else if (command.contains("[refresh]")) {
-                player.updateInventory();
-            } else {
-                player.performCommand(command);
+        if (commands != null) {
+            for (String command : commands) {
+                if (command.contains("[close]")) {
+                    player.closeInventory();
+                } else if (command.contains("[player]")) {
+                    String rawCMD = command.replace("[player] ", "");
+                    player.performCommand(rawCMD.replace("%player%", player.getName()));
+                } else if (command.contains("[console]")) {
+                    String rawCMD = command.replace("[console] ", "");
+                    NotMenus.getInstance().getServer().dispatchCommand(NotMenus.getInstance().getServer().getConsoleSender(), rawCMD.replace("%player%", player.getName()));
+                } else if (command.contains("[refresh]")) {
+                    player.updateInventory();
+                } else {
+                    player.performCommand(command);
+                }
             }
-        }
-
-        for (String message : messages) {
-            player.sendMessage(message);
+        } else if (messages != null) {
+            for (String message : messages) {
+                player.sendMessage(message);
+            }
         }
     }
 }
